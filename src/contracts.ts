31930d04a1cdaa30e1ad5f38e504e8b8bc05551d
Harirak <meen_harirak@hotmail.com>
2021-11-10 23:52:10 +0700

    added time and some indicators

--- src/contracts.ts
+++ src/contracts.ts
@@ -83,28 +83,34 @@ export const REQUEST_INDICATORS = [
   'Pivot.M.Demark.R1{}',
   'open{}',
   'P.SAR{}',
+  'BB.lower{}',
+  'BB.upper{}',
+  'AO[2]{}',
+  'volume{}',
+  'change{}',
+  'low{}',
+  'high{}',
 ];
 
 export enum EXCHANGES_ENUM {
-  'NASDAQ' = 'NASDAQ',
   'FX_IDC' = 'FX_IDC',
-  'NYSE' = 'NYSE',
   'BINANCE' = 'BINANCE',
-  'BITTREX' = 'BITTREX',
 }
 
 export enum SCREENERS_ENUM {
-  'forex' = 'forex',
-  'cfd' = 'cfd',
   'crypto' = 'crypto',
+  'forex' = 'forex',
 }
 
 export enum INTERVALS_ENUM {
   '1m' = '1m',
   '5m' = '5m',
   '15m' = '15m',
+  '30m' = '30m',
   '1h' = '1h',
+  '2h' = '2h',
   '4h' = '4h',
+  '1d' = '1d',
   '1W' = '1W',
   '1M' = '1M',
 }
@@ -127,8 +133,11 @@ export const INTERVALS_SCHEMA = {
   '1m': '|1',
   '5m': '|5',
   '15m': '|15',
+  '30m': '|30',
   '1h': '|60',
+  '2h': '|120',
   '4h': '|240',
+  '1D': '|1d',
   '1W': '|1W',
   '1M': '|1M',
 };
